54a8bff17e1b1fdaf5c0209385e8db13d4a5b50c
achmadnabilfikri <achmadnabil1999@gmail.com>
2024-12-31 03:44:46 -0500

    update folder followers count

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -1,11 +1,23 @@
-let count =0;
-
-function increaseCount() {
-    count++;
-    countDisplay();
-}
+let count = 0;
 
 function countDisplay() {
     document.getElementById("countDisplay").innerHTML= count;
 }
 
+function increaseCount() {
+    count++;
+    countDisplay();
+    checkCountValue();
+}
+
+function checkCountValue() {
+    if (count===30) {
+        alert("Your instagram follower has gain 30, kepp it up");
+    } 
+    else if(count===40) {
+        alert("your instgram follower has gain 40, keep it up");
+    } 
+    else if(count===50) {
+        alert("your instgram follower has gain 50, keep it up");
+    } 
+}
