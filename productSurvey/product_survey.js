27082140f7758e532cb2fb8cd89f2dabce11263d
achmadnabilfikri <achmadnabil1999@gmail.com>
2025-01-05 10:30:42 -0500

    upload product survey folder

---
+++ productSurvey/product_survey.js
@@ -0,0 +1,4 @@
+const userName = document.getElementById("name").value;
+const age = document.getElementById("age").value;
+const email = document.getElementById("email");
+const job = document.getElementById("j")
