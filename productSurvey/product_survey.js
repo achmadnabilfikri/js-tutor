a26c5f31eecf0321096f348d33428f9a84aa7584
achmadnabilfikri <achmadnabil1999@gmail.com>
2025-01-06 00:47:14 -0500

    update product survey

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -1,4 +1,41 @@
-const userName = document.getElementById("name").value;
-const age = document.getElementById("age").value;
-const email = document.getElementById("email");
-const job = document.getElementById("j")
+
+
+
+
+function display() {
+
+    let userName = document.getElementById("name").value;
+    let age = document.getElementById("age").value;
+    let email = document.getElementById("email").value;
+    let job = document.getElementById("job").value;
+    let designation = document.getElementById("designation").value;
+    let productType = document.getElementById("productType").value;
+    let feedback = document.getElementById("feedbackText").value;
+    
+    //display
+    document.getElementById("userName").innerHTML= userName;
+    
+    document.getElementById    
+    ("userAge").innerHTML=age;
+    
+    document.getElementById    
+    ("userEmail").innerHTML=email;
+
+    document.getElementById    
+    ("userJob").innerHTML=job;
+    
+    document.getElementById   
+    ("userDesignation").innerHTML=designation;
+
+    document.getElementById   
+    ("userProductChoice").innerHTML=productType;
+
+    document.getElementById("userFeedback").innerHTML=feedback;
+
+    document.getElementById("userInfo").style.display = "block";
+}
+
+const button = document.getElementById("submitButton");
+
+button.onclick= display;
+
