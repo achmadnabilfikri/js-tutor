7c6fb68080dae07b0884a102a266625c865c73be
achmadnabilfikri <achmadnabil1999@gmail.com>
2025-01-09 05:01:26 -0500

    update folder restaurant menu

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
 const breakfastMenu = ["Pancakes", "Egg benedict", "Oatmeal", "Frittata"];
 const maincourseMenu = ["Steak", "Pasta", "Burger", "Salmon"];
-const dessert = ["Cake", "Ice Cream", "Pudding", "Fruit Salad"];
+const dessertMenu = ["Cake", "Ice Cream", "Pudding", "Fruit Salad"];
 
 // breakfast menu looping 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join("");
@@ -8,8 +8,15 @@ document.getElementById("breakfastMenuItems").innerHTML= breakfastMenuItemsHTML;
 
 
 //maincourse looping
-let maincourseItem = "";
+let maincourseItem = '';
 maincourseMenu.forEach((item, index) => {
     maincourseItem += `<p> Item ${index + 1}: ${item} </p>` ;
 });
-document.getElementById("maincoursemenuItems").innerHTML = maincourseItem;
+document.getElementById("maincourseMenuItems").innerHTML = maincourseItem;
+
+//dessert looping
+let dessertItem ='';
+for (let i=0; i < dessertMenu.length; i++) {
+    dessertItem += `<p> item ${i + 1}: ${dessertMenu[i]}</p>`;
+}
+document.getElementById("dessertMenuItems").innerHTML = dessertItem;
